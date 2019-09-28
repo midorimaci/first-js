608841cfea72029132b7f1b4403c8c3b9380ba78
midorimaci <55018122+midorimaci@users.noreply.github.com>
2019-09-28 18:42:43 +0300

    Add files via upload

---
+++ first-js-master/first-js/script.js
@@ -0,0 +1,15 @@
+var myCurrentAge = "MyCurentAge?";
+var today = "16";
+
+console.log(typeof today);
+
+if (today === "25") {
+  console.log("I am over 25 years old");
+} else {
+  console.log("I am under 25");
+}
+
+console.log(myCurrentAge);
+console.log(myCurrentAge + "My first app");
+console.log(myCurrentAge + "Second app");
+console.log(myCurrentAge + "Third app");
