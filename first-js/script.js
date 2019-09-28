b14988ef82a20f6370d7540f954da8c75b13b834
midorimaci <55018122+midorimaci@users.noreply.github.com>
2019-09-28 18:20:27 +0300

    Add files via upload

---
+++ first-js/script.js
@@ -0,0 +1,15 @@
+var helloWorld = "Hello World!"
+var today = "Sat";
+
+console.log(typeof today)
+
+if (today === "Sat"){
+    console.log("Today is saturday");
+} else {
+    console.log("Whatever");
+}
+
+console.log(helloWorld);
+console.log(helloWorld + "My first app");
+console.log(helloWorld + "Second app");
+console.log(helloWorld + "Third app");
